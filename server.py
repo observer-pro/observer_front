31f7b6b1adaddf64f226d7753caba57896b6fe20
DmitryTell <dmitrij.teleganov@gmail.com>
2023-09-09 14:56:30 +0300

    For deploy 14:56 09.09.23

---
+++ server.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+handler_object = MyHttpRequestHandler
+
+PORT = 80
+
+if __name__ == "__main__":
+    try:
+        with socketserver.TCPServer(("", PORT), handler_object) as my_server:
+            my_server.serve_forever()
+    except PermissionError:
+        print(f"Port {PORT} requires superuser privileges. Try running the script with sudo.")
