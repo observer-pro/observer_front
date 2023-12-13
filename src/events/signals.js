650440ff54a042ae9cf84a83b9a343c66f01b710
DmitryTell <dmitrij.teleganov@gmail.com>
2023-12-13 19:40:12 +0300

    Steps status afused

--- src/events/signals.js
+++ src/events/signals.js
@@ -2,6 +2,7 @@ import socket from "../components/socket.js";
 import { context, appElement, codeElement } from "../main.js";
 import { renderApp } from "../render.js";
 import hljs from "../components/hljs.js";
+import { user_storeage } from "../components/user-storeage.js";
 
 export const getStepsStatus = () => {
     socket.on("steps/status", (data) => {
@@ -10,9 +11,13 @@ export const getStepsStatus = () => {
 
         context.room.users.forEach((user) => {
             if (user.id === data.user_id) {
-                context.currentSteps = Object.values(data.steps);
+                user_storeage[user.id].steps = Object.values(data.steps);
             }
         });
+
+        if (context.activeUserId) {
+            context.currentSteps = [...Object.values(data.steps)];
+        }
 
         renderApp(appElement, context);
 
