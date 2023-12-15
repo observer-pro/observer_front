ba0b227e9d03ebc03814efbec363e5bc644b5794
DmitryTell <dmitrij.teleganov@gmail.com>
2023-12-15 23:20:00 +0300

    Sprint 4 complete

---
+++ src/components/changed-files.js
@@ -0,0 +1,14 @@
+export const getChangedFile = (context, filenames) => {
+    context.filetree.files.forEach((file) => {
+        if (filenames.includes(file.name)) {
+            file.isChanged = true;
+        }
+    });
+    context.filetree.dirs.forEach((dir) => {
+        context.filetree[dir].files.forEach((file) => {
+            if (filenames.includes(file.name)) {
+                file.isChanged = true;
+            }
+        });
+    });
+};
